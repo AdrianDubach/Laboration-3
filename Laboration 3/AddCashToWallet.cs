0895866461668fdc79558c1e3c81950f5b44d3c8
Adrian <Adrian@LAPTOP-IIUB3F29>
2017-10-04 22:46:16 +0200

    Råkade jobba vidare på spinner via denna branch..

--- Laboration 3/AddCashToWallet.cs
+++ Laboration 3/AddCashToWallet.cs
@@ -8,16 +8,18 @@ namespace Laboration_3
 {
     class AddCashToWallet
     {
-        public static void AddCashToPlayWith(Player p)
+        public static double AddCashToPlayWith(Player p)
         {
             Console.WriteLine("How much money do you want to deposit to your wallet " + p.Name + "?");
             while (p.PlayerWallet.CashCheck() <= 0)
             {
                 double.TryParse(Console.ReadLine(), out double startUpCash);
                 p.AddCash(startUpCash);
+                
             }
             Console.Clear();
             Console.WriteLine("Good luck " + p.Name + "! You have " + p.PlayerWallet.CashCheck() + " money in your wallet!");
-        }
+            return p.PlayerWallet.CashCheck();
+        } 
     }
 }
--- Laboration 3/Spinner.cs
+++ Laboration 3/Spinner.cs
@@ -8,6 +8,9 @@ namespace Laboration_3
 {
     class Spinner
     {
+        private static double moneySpent;
+        private static double moneyLost;
+        private static double moneyWon;
         private static string input;
         private static double playerBet;
         private static double winnings;
@@ -16,51 +19,61 @@ namespace Laboration_3
         public static string Spin(Player p)
         {
             if (p.PlayerWallet.CashCheck() <= 0)
-                AddCashToWallet.AddCashToPlayWith(p);
+              moneySpent = moneySpent + AddCashToWallet.AddCashToPlayWith(p);
 
 
-                while (p.PlayerWallet.CashCheck() > 0)
+            while (input != "N" || input != "NO")
+            {
+
+                Console.WriteLine("Do you want to spin, " + p.Name + "? Yes/No");
+                do input = Console.ReadLine().ToUpper();
+                while ((input == "Y" || input == "YES" || input == "N" || input == "NO") == false);
+                if (input == "Y" || input == "YES")
                 {
+                    Console.Clear();
+                    Console.WriteLine("Balance: " + p.PlayerWallet.CashCheck());
+                    Console.Write("Place a bet:");
+                    do input = Console.ReadLine();
+                    while (!double.TryParse(input, out ref double i));
+                    playerBet = double.Parse(input);
+                    moneyLost = moneyLost + playerBet;
+                    if (playerBet <= p.PlayerWallet.CashCheck())
+                    {
+                        p.WithdrawCash(playerBet);
+                        Console.WriteLine();
+                        winnings = GameBoard.Game(playerBet);
+                        if (winnings > 0)
+                            Console.WriteLine("You won " + winnings + ", congratulations " + p.Name + "!");
+                        p.AddCash(winnings);
+                        moneyWon = moneyWon + winnings;
+                    }
+                    else
+                    { Console.WriteLine("Not enough cash to bet that high!"); }
 
-                    Console.WriteLine("Do you want to spin, " + p.Name + "? Yes/No");
-                    do input = Console.ReadLine().ToUpper();
-                    while ((input =="Y" || input == "YES" ||input == "N" ||input == "NO") == false);
+                    Console.WriteLine("you have " + p.PlayerWallet.CashCheck() + " money left!");
+                }
+                if (input == "N" || input == "NO")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Thank you for playing, " + p.Name + "!");
+                    if (moneyLost > moneyWon)
+                    return ("You spent: " + moneySpent + " You Lost: " + moneyLost + " You won: " + moneyWon + ". Better luck next time, " + p.Name);
+                    if (moneyWon > moneyLost)
+                        return ("You spent: " + moneySpent + " You Lost: " + moneyLost + " You won: " + moneyWon + ". Good run, " + p.Name + " You should be crowned The king of " +p.Country + " with the amount of cash you just won!");
+                }
+                if (p.PlayerWallet.CashCheck() <= 0)
+                {
+                    Console.WriteLine("Oh no " + p.Name + "! You lost all your money. Do you wish to insert more? Yes/No");
+                    input = Console.ReadLine().ToUpper();
                     if (input == "Y" || input == "YES")
-                    {
-                        Console.Clear();
-                        Console.WriteLine("Balance: " + p.PlayerWallet.CashCheck());
-                        Console.Write("Place a bet:");
-                        do input = Console.ReadLine();
-                        while (!double.TryParse(input, out ref double i));
-                        playerBet = double.Parse(input);
-                        if (playerBet <= p.PlayerWallet.CashCheck())
-                        {
-                            p.WithdrawCash(playerBet);
-                            Console.WriteLine();
-                            winnings = GameBoard.Game(playerBet);
-                            if (winnings > 0)
-                                Console.WriteLine("You won " + winnings + ", congratulations " + p.Name + "!");
-                            p.AddCash(winnings);
-                        }
-                        else
-                        { Console.WriteLine("Not enough cash to bet that high!"); }
-
-                        Console.WriteLine("you have " + p.PlayerWallet.CashCheck() + " money left!");
-                    }
+                        moneySpent = moneySpent + AddCashToWallet.AddCashToPlayWith(p);
                     if (input == "N" || input == "NO")
-                    {
-                        Console.Clear();
-                        Console.WriteLine("Thank you for playing, " + p.Name + "!");
-                        return ("You walk away with " + p.PlayerWallet.CashCheck() + " money!");
-                    }
-                    if (p.PlayerWallet.CashCheck() <= 0)
-
-                    Console.WriteLine("Oh no " + p.Name + "! You lost all your money. Do you wish to insert more?");
+                        return ("You walk away a broken man, " + p.PlayerWallet.CashCheck() + " money to your name! FeelsBadMan.");
+                }
             }
+            return ("You spent: " + moneySpent + " You Lost: " + moneyLost + " You won: " + moneyWon);
 
-                return ("You walk away a broken man, " + p.PlayerWallet.CashCheck() + " money to your name! FeelsBadMan.");
-            
-        }
         }
     }
+}
 
